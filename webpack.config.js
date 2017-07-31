4dd0db6a8c7de0183ba1a40819d9617840ad70fa
Vincent Khougaz <vincent@khougaz.com>
2017-07-31 16:44:40 -0700

    Project framework

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist/'),
+        publicPath: '/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+}
